a5e710b7c6c7360c00e96c99aa9290425238310f
Roman Fedotenko <fedotenko@gmail.com>
2020-12-06 12:15:37 +0700

    Day 6

---
+++ 06/index.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const input = fs.readFileSync("./input.txt", { encoding: "utf8" });
+const transformed = input.replace(/[F|L]/g, 0).replace(/[B|R]/g, 1);
+const groups = transformed.split("\n\n");
+
+let first = 0;
+let second = 0;
+
+for (let group of groups) {
+  const answers = group.split("\n");
+  const l = answers.length;
+  const h = {};
+  for (let answer of answers) {
+    for (let i = 0; i < answer.length; i++) {
+      h[answer[i]] = h[answer[i]] ? h[answer[i]] + 1 : 1;
+    }
+  }
+  first += Object.keys(h).length;
+  for (let key of Object.keys(h)) {
+    if (h[key] >= l) {
+      second += 1;
+    }
+  }
+}
+
+console.log("1:", first);
+console.log("2:", second);
